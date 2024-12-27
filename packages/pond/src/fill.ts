1095e824a20c3aae2a3ef1e7124f098bb752c2d2
Frederik Leonhardt <frederik.leonhardt@gmail.com>
2024-12-28 01:17:17 +1300

    pond: Compat fix for lodash types
    - Fix type definition in fill.js causing a build error after updating to
      newer lodash types.

--- packages/pond/src/fill.ts
+++ packages/pond/src/fill.ts
@@ -32,7 +32,7 @@ export class Fill<T extends Key> extends Processor<T, T> {
 
     // Internal state
     private _previous: Event<T>;
-    private _keyCount;
+    private _keyCount: { [key: string]: number };
     private _lastGoodLinear;
     private _linearFillCache;
 
