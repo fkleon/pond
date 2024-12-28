90d570a48c0a83412dbb9d85faef403f54286938
Frederik Leonhardt <frederik.leonhardt@gmail.com>
2024-12-28 21:58:30 +1300

    website: update to react-scripts v2.x

--- packages/website/src/site/App.test.js
+++ packages/website/src/site/App.test.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import renderer from 'react-test-renderer';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+window.scrollTo = jest.fn();
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
 });
 
-it('renders a snapshot', () => {
-  const tree = renderer.create(<App/>).toJSON();
-  expect(tree).toMatchSnapshot();
+it("renders a snapshot", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
 });
